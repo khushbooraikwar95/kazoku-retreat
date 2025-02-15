e91671786f8acf395ddd85695f9073ba77a2ffe5
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2025-02-15 12:46:00 +0530

    Home and About page implementation

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,36 @@
-import { Children } from "react";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
+export const metadata = {
+  title: {
+    template: "%s / Kazoku-Retreat",
+    default: "Welcome / Kazoku-Retreat",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Navigation />
-        <main>{children}</main>
-        <footer>copyright by khushboo</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
---
+++ starter/components/Logo.js
@@ -0,0 +1,12 @@
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-4 z-10">
+      <img height="60" width="60" alt="Kazoku-Retreat logo" />
+      <span className="text-xl font-semibold text-primary-100">
+        Kazoku-Retreat
+      </span>
+    </a>
+  );
+}
+
+export default Logo;
